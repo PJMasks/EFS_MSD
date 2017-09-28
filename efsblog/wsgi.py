fd6da2339de907a46e00ee63f0b99deb0835a65d
Apurva8001 <achimbili@unomaha.edu>
2017-09-27 21:56:40 -0500

    deploying code for a

--- efsblog/wsgi.py
+++ efsblog/wsgi.py
@@ -8,11 +8,15 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
 import os
+
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
-application = DjangoWhiteNoise(application)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "efsblog.settings")
 
+
+from whitenoise.django import DjangoWhiteNoise
+
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
